9ac62b5cc23284d22d3b7816e590ded0b1ba6c22
km4891 <kmorarity@gmail.com>
2020-12-21 17:57:09 -0800

    added thought route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-route');
+const thoughtsRoutes = require('./thoughts-route')
 
 router.use('/user', userRoutes);
+router.use('/thoughts', thoughtsRoutes)
 
 module.exports = router;
---
+++ routes/api/thoughts-route.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+} = require('../../controllers/thoughts-controller');
+
+// api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThoughts);
+// api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtsById)
+    .put(updateThoughts)
+    .delete(deleteThoughts);
+
+    module.exports = router;
