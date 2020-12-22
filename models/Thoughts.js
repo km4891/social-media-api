e44a90ed3d147c1ddf70f146f5dd80a736a30c43
km4891 <kmorarity@gmail.com>
2020-12-21 17:05:14 -0800

    setting up Thoughts

---
+++ models/Thoughts.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        usernmae: {
+            type: String,
+            required: true
+        }
+    },
+        {
+            toJSON: {
+                virtuals: true,
+                getters: true
+            },
+            id: false
+        }
+);
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
